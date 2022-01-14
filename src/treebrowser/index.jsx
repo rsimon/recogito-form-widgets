9f7cb004b94c2c96d4a70cd0da703255b87c520d
Rainer Simon <aboutgeo@gmail.com>
2022-01-14 13:10:32 +0100

    Taxonomy file load + caching

--- src/treebrowser/index.jsx
+++ src/treebrowser/index.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { load as loadJSKOS } from './jskos';
+
+let CACHED_TAXONOMY = null;
+
+const loadTaxonomy = async config => {
+  let loadFn;
+
+  if (config.format.toLowerCase() === 'jskos')
+    loadFn = loadJSKOS;
+  else 
+    throw new `Unsupported taxonomy format: ${config.format}`
+
+  // Switch between different formats based on config later!
+  return fetch(config.src)
+    .then(response => response.json())
+    .then(result => {
+      const taxonomy = loadFn(result);
+
+      CACHED_TAXONOMY = taxonomy;
+      return taxonomy;
+    });
+}
 
 export default config => props => {
 
-  return (
-    <div>Hello World</div>
-  )
+  const [ taxonomy, setTaxonomy ] = useState();
+
+  useEffect(() => {
+    // We don't want to reload every time the editor opens!
+    if (CACHED_TAXONOMY) {
+      setTaxonomy(CACHED_TAXONOMY);
+    } else {
+      loadTaxonomy(config).then(setTaxonomy);
+    } 
+  }, []);
+
+  return taxonomy ?
+    <div>Hello World</div> : <div>LOADING</div>;
 
 }
--- src/treebrowser/jskos/index.js
+++ src/treebrowser/jskos/index.js
@@ -1,7 +1,12 @@
 /**
  * Loads a taxonomy tree from a JSKOS data object.
  * @param {object} json 
+ * 
  */
 export const load = json => {
+
+  console.log('foo', json);
+
+  return json;
 
 }
