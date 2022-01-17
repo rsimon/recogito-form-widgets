942830c45af94d23527301c70e04d9eabddb7a57
Rainer Simon <aboutgeo@gmail.com>
2022-01-17 13:38:23 +0100

    Added React portal for popup treebrowser

--- src/taxonomy/browser/TaxonomyBrowser.jsx
+++ src/taxonomy/browser/TaxonomyBrowser.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 const TaxonomyBrowser = props => {
 
-  return null; // TODO
+  return ReactDOM.createPortal(
+    <div className="r6o-taxonomy r6o-taxonomybrowser">
+      Treebrowser...
+    </div>,
+    document.body
+  );
 
 }
 
