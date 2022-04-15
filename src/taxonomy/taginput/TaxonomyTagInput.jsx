d5e01601d70c1d116f3c0378157d4d6e1ac08136
Rainer Simon <aboutgeo@gmail.com>
2022-04-15 10:40:02 +0200

    Tag delete + tag list styling

--- src/taxonomy/taginput/TaxonomyTagInput.jsx
+++ src/taxonomy/taginput/TaxonomyTagInput.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BiNetworkChart } from 'react-icons/bi';
 import { RiNodeTree } from 'react-icons/ri';
+import { CSSTransition } from 'react-transition-group';
 
 import Autocomplete from '@recogito/recogito-client-core/src/editor/widgets/Autocomplete';
+import { CloseIcon } from '@recogito/recogito-client-core/src/Icons';
 
 const TaxonomyTagInput = props => {
 
+  const [ showDelete, setShowDelete ] = useState(false);
+
   const tags = props.annotation.bodies.filter(b => b.purpose === 'identifying');
+
+  const toggle = tag => () => {
+    if (showDelete === tag) // Removes delete button
+      setShowDelete(false);
+    else
+      setShowDelete(tag); // Sets delete button on a different tag
+  }
+
+  const onDelete = tag => evt => {
+    evt.stopPropagation();
+    props.onRemoveBody(tag);
+  }
 
   const onInputSubmit = ({ uri }) => {
     const term = props.taxonomy.findByURI(uri);
@@ -28,8 +44,16 @@ const TaxonomyTagInput = props => {
       {tags.length > 0 &&
         <ul className="r6o-taglist">
           {tags.map(tag =>
-            <li key={tag.source.id}>
+            <li key={tag.source.id} onClick={toggle(tag)}>
               <span className="r6o-label">{tag.source.label}</span>
+
+              <CSSTransition in={showDelete === tag} timeout={200} classNames="r6o-delete">
+                <span className="r6o-delete-wrapper" onClick={onDelete(tag)}>
+                  <span className="r6o-delete">
+                    <CloseIcon width={12} />
+                  </span>
+                </span>
+              </CSSTransition>
             </li>
           )}
         </ul>
